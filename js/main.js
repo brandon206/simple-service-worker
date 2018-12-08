1cdcfeefe507a0cc735087c408b4b48eb9b130b0
Brandon Park <brandonpark206@gmail.com>
2018-12-07 18:20:03 -0800

    service worker for entire site

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/sw_cached_pages.js')
+      // .register('/sw_cached_pages.js')
+      .register ('/sw_cached_site.js')
       .then( registration => console.log("ServiceWorker registration successful"))
       .catch(error => console.log(`ServiceWorker error: ${error}`));
   } )
